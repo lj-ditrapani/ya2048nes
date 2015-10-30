5f02e129c2106144dbb015547654fb004adfb33b
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2015-10-30 08:25:37 -0400

    Nothing displays on screen in FCEUX
    mednafen crashes with "Error reading CHR ROM data"

--- game.asm
+++ game.asm
@@ -2,34 +2,84 @@
 .aasc "NES", $1A    ; Static NES\n in header
 .byt 1              ; PRG ROM data 1 X 16,384 bytes
 .byt 1              ; CHR ROM data 1 X  8,192 bytes
-.byt %00000001      ; Flags 6, horizontal scroll (vertical mirroring)
-.byt %00000000      ; Flags 7
-.byt 1              ; PRG RAM 1 X 8,192 bytes
-.byt %00000000      ; Flags 9
-.byt %00000000      ; Flags 10
-.dsb 5,0            ; Bytes 11-15 zero filled
 ; Flags 6
 ; bit  0)   0 vertical scroll  --  1 horizontal scroll
 ; bits 1)   Set battery backeg PRG RAM ($6000-7FFF)
 ; bits 2)   Set 512-byte trainer at $7000-$71FF (stored before PRG data)
 ; bit  3)   Set 4-screen VRAM (ignor bits 0-2)
 ; bits 4-7) mapper number
+.byt %00000001      ; Flags 6, NROM mapper
+                    ;          horizontal scroll (vertical mirroring, bit 0)
+.byt %00000000      ; Flags 7
+.byt 1              ; PRG RAM 1 X 8,192 bytes
+.byt %00000000      ; Flags 9
+.byt %00000000      ; Flags 10
+.dsb 5,0            ; Bytes 11-15 zero filled
 
 ; Set PC to $8000
+; $8000 = 32,768 = 32 KB = start of NES PRG ROM
 * = $8000
 
-int_reset:
-    LDA #$FF
+RESET:
+    SEI                 ; SEt Interrupt disable; I flag = 1; disable IRQs
+    CLD                 ; CLear Decimal mode; D flag = 0; Disable decimal mode
+    LDX #%01000000      ; MI-- ---- IRQ inhib flag (I)
+    STX $4017           ; disable APU frame IRQ (APU Frame Counter Register)
+    LDX #$FF
+    TXS                 ; Set up stack; Transfer Index X to Stack Register
+    INX                 ; X = 255 + 1 = 0
+    ; Now store 0 to the following 3 address to clear flags.
+    STX $2000           ; disable NMI
+    STX $2001           ; disable rendering
+    STX $4010           ; Set APU DMC register to 0; disable DMC IRQs IL-- RRRR
+
+    LDA #%00001000      ; GB patterns $0000; Sprite patterns $1000
+    STA $2000
+    LDA #%00011110      ; D4 Sprites visible
+                        ; D3 BG visible
+                        ; D2 No sprite clipping
+                        ; D1 No BG clipping
+                        ; D0 Color display
+    STA $2001
+
+    ; PALETTE
+    ; Set palette colors
+    ; $3F00 - $3F0F )  Background palette
+    ; $3F10 - $3F1F )  Sprite palette
+    ; Set main background color at $3F00
+    LDA #$3F
+    STA $2006
+    LDA #$00
+    STA $2006
+    LDX #$00
+fill_palette:
+    INX
+    STX $2007
+    CPX #$20
+    BNE fill_palette
+
+
+    ; LDA #%10000000      ; intensify blues
+    ; STA $2001
+loop:
+    JMP loop            ; infinite loop
+
 
 ; When vblank or irqbrk interrupts are triggered,
 ; just immediately return from the  interrupt
-int_vblank:
-int_irqbrk:
+VBLANK:
+IRQ:
     rti
 
 
 ; Fill with zeros from current PC to $bffa
 ; (from end of program to end of rom)
-.dsb $bffa-*-16, 0
+; $8000 = 32,768 = 32 KB
+; $BFFA = 49,146 = 48 KB
+; * = end of program (current PC)
+; 16 = size of iNES header in bytes
+.dsb $BFFA-*-16, 0
 
-.word int_vblank, int_reset, int_irqbrk
+.word VBLANK, RESET, IRQ
+
+.bin 0,8192,"char.bin"
